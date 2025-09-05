dff6308eb903466b2d4c058db3069d18f3a3f35b
Marcus <marcus29sfj@gmail.com>
2025-09-05 18:51:02 +0200

    Added warnings nobody reads

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./../styles/globals.css";
 import { Geist } from "next/font/google";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 // ⬇️ Bruk provider fra i18n/messages
 import { I18nProvider } from "@/i18n/messages/I18nProvider";
@@ -12,13 +13,22 @@ export const metadata = {
   title: "Trevl – Dine reiser, dine minner",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="no">
-      <body className={`${geist.variable} bg-background text-foreground font-sans`}>
+      <body
+        className={`${geist.variable} bg-background text-foreground font-sans`}
+      >
         <I18nProvider defaultLang="no">
           <Header />
-          <main className="min-h-screen max-w-6xl mx-auto px-4 py-10">{children}</main>
+          <Analytics />
+          <main className="min-h-screen max-w-6xl mx-auto px-4 py-10">
+            {children}
+          </main>
           <Footer />
         </I18nProvider>
       </body>
